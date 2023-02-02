e3cac5aaf018deb4d79acd2a9f3021b03b8ba561
KIMDONGBEEN <97274814+dd1761@users.noreply.github.com>
2023-02-02 19:52:36 +0900

    23-02-02

--- 01_basic/src/operator/Dice.java
+++ 01_basic/src/operator/Dice.java
@@ -9,9 +9,13 @@ public class Dice {
 		int x = 1, y = 6;
 		dice1 = (int)(Math.random() * (y-x+1) +x);
 		dice2 = (int)(Math.random() * (y-x+1) +x);
+		int sum = dice1 + dice2;
 		
-		System.out.println(Math.random());
-
+		
+		System.out.println("주사위1 : " + dice1 + "\t 주사위2 : " + dice2 );
+		String result = dice1 > dice2 ? "주사위 1 승" : dice1 < dice2  ?  "주사위 2 승" : "무승부";
+		System.out.println(result);
+		System.out.println("주사위의 합 : " + sum);
 	}
 
 }
