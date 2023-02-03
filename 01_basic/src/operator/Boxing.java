19d1e9a3bf557fd2969a310c10600c9ca08a9dc5
KIMDONGBEEN <97274814+dd1761@users.noreply.github.com>
2023-02-03 11:10:06 +0900

    23-02-03

--- 01_basic/src/operator/Boxing.java
+++ 01_basic/src/operator/Boxing.java
@@ -3,6 +3,12 @@ package operator;
 public class Boxing {
     
     public static void main(String[] args) {
-        
+        int a = 25;
+        double b = (double) a / 3;
+
+        String c = "25";
+
+        //int d = (int)c; // error (기본형) 객체형
+        int d = Integer.parseInt(c); // 문자열을 정수로 변환
     }
 }
